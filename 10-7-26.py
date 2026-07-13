16379b1469e0b9cb073d2f7d23ae50ec137f9dc1
lalit-mittal8769 <lalitnetsquaresoftwares@gmail.com>
2026-07-13 11:03:20 +0530

    initial

---
+++ 10-7-26.py
@@ -0,0 +1,90 @@
+#1
+""" num = float(input("Enter number to check: "))
+if num<0:
+    print("number is negative")
+else:
+    print("number is positive") """
+
+#2
+""" num = int(input("Enter number to check: "))
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")  """
+
+#3
+""" age = int(input("Enter age to check: "))
+if age<18:
+    print("not eligible to vote")
+else:
+    print("eligible to vote")  """
+
+#4
+""" num1 = int(input("Enter 1st number to check: "))
+num2 = int(input("Enter 2nd number to check: "))
+if num1>num2:
+    print(f"{num1} is greater")
+else:
+    print(f"{num2} is greater") """
+
+#5
+""" num1 = int(input("Enter 1st number to check: "))
+num2 = int(input("Enter 2nd number to check: "))
+num3 = int(input("Enter 3rd number to check: "))
+if num1>num2:
+    if num1>num3:
+        print(f"{num1} is greater") 
+if num2>num1:
+    if num2>num3:
+        print(f"{num2} is greater") 
+if num3>num2:
+    if num3>num1:
+        print(f"{num3} is greater")  """
+
+#6
+""" Maths = float(input("Enter your marks in Maths Out of 100 : "))
+Science = float(input("Enter your marks in Science Out of 100 : "))
+Computer = float(input("Enter your marks in Computer Out of 100 : "))
+Percentage = (Maths + Science + Computer) / 3
+if Percentage >= 40:
+    print("You have passed.")
+else:
+    print("You have failed.") """
+
+#7
+""" Percentage = float(input("Enter your percentage: "))
+if Percentage >= 90:
+    print("Grade: A")
+elif Percentage >= 80:
+    print("Grade: B")       
+elif Percentage >= 60:
+    print("Grade: C")
+else:
+    print("Grade: D") """
+
+#8
+""" acc= 2049
+pin= 1234
+ent_acc= int(input("enter account number"))
+ent_pin= int(input("Enter pin: "))
+if acc==ent_acc and pin==ent_pin:
+    print('PIN verified')
+else:
+    print("Wrong account number or PIN") """
+
+#9
+""" username= 'abc'
+password = 'abd56*'
+ent_username= input('Enter username:')
+ent_password= input('Enter password: ')
+if username==ent_username and password==ent_password:
+    print('Login successful')
+else:
+    print('Login failed') """
+
+#10
+""" percentage = float(input("Enter your percentage: "))
+if percentage>95:
+    print("You are eligible for the scholarship.")
+else:
+    print("You are not eligible for the scholarship.") """
---
+++ 9-7-26.py
@@ -0,0 +1,97 @@
+#1
+""" Name = input('Enter Name')
+Age = input('Enter Age')
+Course = input('Enter Course')
+College = input('Enter College')
+Email = input('Enter Email')
+Phone = input('Enter Phone')
+City = input('Enter City')
+
+print('=========================================\n          STUDENT PROFILE\n=========================================\n')
+print(f"Name : {Name}\nAge : {Age}\nCourse : {Course}\nCollege : {College}\nEmail : {Email}\nPhone : {Phone}\nCity : {City}\n")
+print('=========================================')"""
+
+#2
+""" EmployeeName = input('Enter employee Name')
+CompanyName = input('Enter company Name')
+Designation = input('Enter Designation')
+JoiningDate = input('Enter joining date')
+Salary = input('Enter salary')
+print(f"Dear {EmployeeName},\nCongratulations!\nYou have been selected as {Designation} at {CompanyName}.\nJoining Date : {JoiningDate}\nSalary       : ₹{Salary}\nWe welcome you to our team.\nRegards,\nHR Department")"""
+
+#3
+""" Burger=120
+Pizza=250
+Pasta=180
+Coffee=80
+Cold_Drink=50
+print(f"==============================\n\tNETSQUARE CAFE\n==============================\n")
+print(f"1. Burger : ₹{Burger}\n2. Pizza : ₹{Pizza}\n3. Pasta : ₹{Pasta}\n4. Coffee : ₹{Coffee}\n5. Cold Drink : ₹{Cold_Drink}\n")
+print(f"==============================")"""
+
+#4
+""" Name=input("Enter your name: ")
+Age=int(input("Enter your age: "))
+Qualification=input("Enter your qualification: ")
+Skills=input("Enter your skills: ")
+Experience=int(input("Enter your experience in years: "))
+City=input("Enter your city: ")
+Email=input("Enter your email: ")
+print(f"==============================\n\tEMPLOYEE PROFILE\n==============================")
+print(f"Name : {Name}\nAge : {Age}\nQualification : {Qualification}\nSkills : {Skills}\nExperience : {Experience} years\nCity : {City}\nEmail : {Email}\n")"""
+
+#5
+""" Customer_Name=input("Enter Customer Name :")
+Account_Number=int(input("Enter Account Number :"))
+Branch=input("Enter Branch :")
+IFSC=input("Enter IFSC Code :")
+Balance=float(input("Enter Balance :"))
+print("========================================\n\tPASSBOOK\n========================================")
+print(f"\nAccount Holder : {Customer_Name}\nAccount Number : {Account_Number}\nBranch     : {Branch}\nIFSC Code  : {IFSC}\nBalance    : ₹{Balance:,.2f}\n")
+print("========================================") """
+
+#6
+""" print("Python\n\tVariables\n\t\tData Types\n\t\t\tInput\n\t\t\t\tOutput")"""
+
+#7
+""" print("Rahul said,\n\"I love learning Python\"\nTeacher replied,\n'Practice every day'")"""
+
+#8
+""" name = "Rahul" #name error as it is string
+
+Age = 22 #type error
+
+print("Name :" + name)
+
+print("Age :" + Age)
+
+height = 5.8 #type error
+
+print(height)
+
+print("Welcome")  #syntax error """
+
+#9
+""" print('student') #syntax
+#Code 2
+print(20 + 30) #type
+#Code 3
+marks = int("123") #value
+#Code 4
+if True: #identation 
+    print("Hello")
+#Code 5
+print("Python") #syntax """
+
+#10
+""" Name=input("Enter Name :")
+Age=int(input("Enter Age :"))
+Course=input("Enter Course :")
+College=input("Enter College :")
+City=input("Enter City :")
+Mobile=int(input("Enter Mobile :"))
+Email=input("Enter Email :")
+LinkedIn_Username=input("Enter LinkedIn Username :")
+print("\n==================================================\n\t\tMY DIGITAL PROFILE\n==================================================\n\n")
+print(f"Name\t\t: {Name}\nAge\t\t: {Age}\nCourse\t\t: {Course}\nCollege\t\t: {College}\nCity\t\t: {City}\nPhone\t\t: +91 {Mobile}\nEmai\t\t: {Email}\nLinkedIn\t: {LinkedIn_Username}")
+print("==================================================\n\tThank You For Visiting My Profile\n==================================================\n\n") """
